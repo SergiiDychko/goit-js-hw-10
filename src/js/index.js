0c44536365ac6cb00853fc0cf8c1ec6a0b08520d
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-26 15:44:13 +0300

    відновив видалені файли

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries';
+import '../css/styles.css';
+import { fetchCountries } from './fetchCountries';
 import { Notify } from 'notiflix';
 import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
